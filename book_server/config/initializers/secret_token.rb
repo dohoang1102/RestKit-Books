e16f968117e394f773627d914617d64ebd6ca79c
John Ryan <john.ryan@arturia.com>
2012-07-27 14:42:19 +0200

    made book server

---
+++ book_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookServer::Application.config.secret_token = '5499da2fc9d88440be06abbd8f3e96ed357ebf2a736a8632975994599535d8ab1efcdeb2ab0ebf3cc5b2c67d946d5f5f76f55870a47d467ecab0a5cf275be229'
